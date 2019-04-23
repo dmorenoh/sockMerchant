92d5f5f2b2f425131441b9dfd6153e789fa3d508
David Moreno <d.moreno@mytaxi.com>
2019-04-24 01:49:39 +0200

    Fixed solution

--- src/main/kotlin/com/myapps/sockmerchant/SocksMerchantOperator.kt
+++ src/main/kotlin/com/myapps/sockmerchant/SocksMerchantOperator.kt
@@ -1,4 +1,13 @@
 package com.myapps.sockmerchant
 
 class SocksMerchantOperator {
+    fun sockMerchant(socks: List<Int>): Int {
+//        return socks
+//                .groupingBy { it }
+//                .eachCount()
+//                .filter { it.value > 1 }
+//                .mapValues { it.value.div(2) }.values.sum()
+        return socks
+                .groupBy { it }.values.sumBy { it.size.div(2) }
+    }
 }
--- src/test/groovy/com/myapps/sockmerchant/SocksMerchantOperatorSpec.groovy
+++ src/test/groovy/com/myapps/sockmerchant/SocksMerchantOperatorSpec.groovy
@@ -1,4 +1,22 @@
 package com.myapps.sockmerchant
 
-class SocksMerchantOperatorSpec {
+import com.blogspot.toomuchcoding.spock.subjcollabs.Subject
+import spock.lang.Specification
+
+class SocksMerchantOperatorSpec extends Specification {
+    @Subject
+    SocksMerchantOperator socksMerchantOperator
+
+    def "should return number of matching pairs"() {
+        given: "a list of socks"
+            def array = socks
+        when: "calculates number of pairs"
+            def pairs = socksMerchantOperator.sockMerchant(array)
+        then: "return number of pairs"
+            pairs == expected
+        where:
+            socks                                | expected
+            [10, 20, 20, 10, 10, 30, 50, 10, 20] | 3
+            [1, 2, 1, 2, 1, 3, 2]                | 2
+    }
 }
